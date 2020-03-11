14d3e63d80f5893cd3daf3acb99835f6d8e3b4e8
Kevin Langat <kevnlan11@gmail.com>
2020-03-11 17:39:43 +0300

    First Commit

---
+++ app/src/main/java/com/kevnlan/application/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.kevnlan.application.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startbtn = findViewById(R.id.startBtn);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent categoryIntent = new Intent(MainActivity.this,CategoriesActivity.class);
+                startActivity(categoryIntent);
+            }
+        });
+}
+}
